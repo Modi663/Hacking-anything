968c86feee532e8ac2a9bfe6bc8a60cc044e38c2
Modi663 <berbeca.vadik67@gmail.com>
2021-11-01 23:17:24 +0200

    Add pentagon.py

---
+++ pentagon.py
@@ -0,0 +1,21 @@
+from tqdm import tqdm
+from time import sleep
+import random
+
+x = input("Введите кого взломать(id аккаунта, имя фамилия и т.п))>> ")
+print(f"Взлом {x}")
+
+for i in tqdm(range(100)):
+    sleep(random.random())
+
+x = input(f"{x} ВЗЛОМАН! Что сделать? (Сьесть/Дать пароль)>> ").lower()
+if x == "сьесть":
+    for i in tqdm(range(5)):
+        sleep(random.random())
+    print(f"{x} успешно съеден")
+elif x == "дать пароль":
+    print("Ха-ха, пошел нахуй")
+else:
+    print("Пиши нормально")
+
+
